15a274e2b6183c0dd56ab1a1bedfd4da23afa3ad
DatOnTheRoad <tandatthtt@gmail.com>
2026-03-31 23:52:56 +0700

    feature: character movement animation & place bomb

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class BombController : MonoBehaviour
+{
+    [SerializeField] GameObject bombPrefab;
+    // Bomb settings
+    [SerializeField] readonly float bombFuseTime = 3f;
+    [SerializeField] readonly int bombAmount = 3;
+    [SerializeField] int bombRemaining;
+    [SerializeField] readonly float bombChargeTime = 3f; 
+    [SerializeField] float bombCurrentChargeTime = 0f;
+
+    private void OnEnable()
+    {
+        bombRemaining = bombAmount;
+    }
+    private void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null) return; 
+        bombCurrentChargeTime += Time.deltaTime;    
+        if(bombCurrentChargeTime >= bombChargeTime) 
+        {
+            bombCurrentChargeTime = 0f;
+            bombRemaining = bombRemaining >= bombAmount ? bombAmount : bombRemaining + 1;  
+            Debug.Log($"Bomb recharged! Remaining bombs: {bombRemaining}"); 
+        }
+        if (keyboard.spaceKey.wasPressedThisFrame && bombRemaining > 0) 
+        {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    public IEnumerator PlaceBomb() 
+    {
+        GameObject bomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+        bombRemaining--;
+       
+        yield return new WaitForSeconds(bombFuseTime);
+        // fuse time is over, bomb explodes
+        Destroy(bomb);  
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bomb"))
+        {
+            Debug.Log("Player exited bomb area, making it solid");
+            collision.isTrigger = false;
+        }
+    }
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,13 +4,14 @@ using UnityEngine.InputSystem;
 public class MovementController : MonoBehaviour
 {
     public new Rigidbody2D rigidbody;
-    
+    public Animator animator;
     [SerializeField] private float speed = 5f;  
     Vector2[] directions = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
     Vector2 currentDirection = Vector2.zero;    
     void Start()
     {
         this.rigidbody = GetComponent<Rigidbody2D>(); 
+        this.animator = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
@@ -18,7 +19,8 @@ public class MovementController : MonoBehaviour
     {
         var keyboard = Keyboard.current;
         if (keyboard == null) return; 
-        currentDirection = getDirection(keyboard);  
+        currentDirection = getDirectionByKey(keyboard);  
+        UpdateAnimation(currentDirection);
     }
     void FixedUpdate()
     {
@@ -27,20 +29,41 @@ public class MovementController : MonoBehaviour
 
         rigidbody.MovePosition(position + traslation); 
     }
-    Vector2 getDirection(Keyboard keyboard) 
+    Vector2 getDirectionByKey(Keyboard keyboard) 
     {
         switch (true) 
         {
-            case true when keyboard.wKey.isPressed:
+            case true when keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed:
                 return directions[0];
-            case true when keyboard.sKey.isPressed:
+            case true when keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed:
                 return directions[1];
-            case true when keyboard.aKey.isPressed:
+            case true when keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed:
                 return directions[2];
-            case true when keyboard.dKey.isPressed:
+            case true when keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed:
                 return directions[3];
             default:
                 return Vector2.zero;
         }
+    }
+    void UpdateAnimation(Vector2 direction) 
+    {
+        switch (true) 
+        {
+            case true when direction == directions[0]:
+                animator.SetTrigger("Go Up");
+                break;
+            case true when direction == directions[1]:
+                animator.SetTrigger("Go Down");
+                break;
+            case true when direction == directions[2]:
+                animator.SetTrigger("Turn Left");
+                break;
+            case true when direction == directions[3]:
+                animator.SetTrigger("Turn Right");
+                break;
+            default:
+                animator.SetTrigger("Idle");    
+                break;
+        }   
     }
 }
